659b28ed25b251a369128d40d7c8aaf0878b4b73
Preetham Goud <preethamsureshgoud@gmail.com>
2018-03-25 08:35:55 +0530

    Login view (Pull #1)

--- vue-chatterbox-src/src/App.vue
+++ vue-chatterbox-src/src/App.vue
@@ -1,19 +1,35 @@
 <template>
-  <div id="app">{{ name }}</div>
+  <div id="app">
+    <nav class="navbar fixed-top " style="background-color: #AED09E;">
+        <span id="nav_text" class="navbar-brand mb-0 mx-auto unselectable">ChatterBox</span>
+    </nav>
+    <Login></Login>
+    <router-view/>
+  </div>
 </template>
 
 <script>
+import Login from './components/Login'
+
 export default {
   name: 'App',
-  data() {
-    return {
-      name: "DDD"
-    }
+  components :{
+    Login
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Pacifico');
+
+.navbar-brand {
+    color: rgb(248, 248, 248);
+    text-shadow: 2px 1px rgba(215, 212, 219, 0.979);
+    font-family: 'Pacifico';
+    font-size: 170%;
+    font-weight: 300;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -21,5 +37,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  background-image: "/src/images/background.jpg";
+}
+
+.unselectable {
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
 }
 </style>
---
+++ vue-chatterbox-src/src/components/Login.vue
@@ -0,0 +1,146 @@
+<template>
+    <b-container>
+        <b-card>
+        <b-tabs id="tabs">
+            <b-tab title="Login" active>
+                <div>
+                    <b-form @submit="onSubmit">
+                        <div id="textalign">
+                            <b-form-group id="emailGroup"
+                                    label="Email address:"
+                                    label-for="email"
+                                    class="unselectable">
+                        <b-form-input id="email"
+                                    type="email"
+                                    v-model="form.email"
+                                    required
+                                    placeholder="Enter email">
+                        </b-form-input>
+                    </b-form-group>
+                    <b-form-group id="passwordGroup"
+                                    label="Your Password:"
+                                    label-for="password"
+                                    class="unselectable">
+                        <b-form-input id="password"
+                                    type="password"
+                                    v-model="form.password"
+                                    required
+                                    placeholder="Enter password">
+                        </b-form-input>
+                    </b-form-group>
+                        </div>
+                    <b-button type="submit" variant="primary">Login</b-button>
+                    </b-form>
+                </div>
+            </b-tab>
+            <b-tab title="Sign Up" >
+               <div>
+                    <b-form @submit="onSubmit">
+                        <div id="textalign">
+                            <b-form-group id="newEmailGroup"
+                                    label="Email address:"
+                                    label-for="new_email"
+                                    description="We'll never share your email with anyone else."
+                                    class="unselectable">
+                        <b-form-input id="new_email"
+                                    type="email"
+                                    v-model="form.email"
+                                    required
+                                    placeholder="Email address">
+                        </b-form-input>
+                    </b-form-group>
+                    <b-form-group id="nameGroup"
+                                    label="Your Name:"
+                                    label-for="name"
+                                    class="unselectable">
+                        <b-form-input id="name"
+                                    type="text"
+                                    v-model="form.name"
+                                    required
+                                    placeholder="Enter name">
+                        </b-form-input>
+                    </b-form-group>
+                    <b-form-group id="new_password"
+                                    label="Create Password:"
+                                    label-for="new_password"
+                                    class="unselectable">
+                        <b-form-input id="new_password"
+                                    type="password"
+                                    v-model="form.password"
+                                    required
+                                    placeholder="New password">
+                        </b-form-input>
+                    </b-form-group>
+                        </div>
+                    <b-button type="submit" variant="success">Sign Up</b-button>
+                    </b-form>
+                </div>
+            </b-tab>
+        </b-tabs>
+        </b-card>
+    </b-container>
+</template>
+
+<script>
+
+export default {
+  name: 'Login',
+  data() {
+      return {
+          
+          form: {
+              name: '',
+              email: '',
+              password:'',
+          }
+      }
+  },
+  methods: {
+      onSubmit: function() {
+          alert('HELLOO')
+      }
+  }
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css?family=Nunito:600');
+
+.card-body {
+    box-sizing: 100px 200px;
+    box-shadow: 0 0px 0px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    -webkit-transition: box-shadow 1s;
+    transition: box-shadow 1s;
+}
+.card-body:hover {
+    box-shadow: 0 12px 12px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
+
+#tabs {
+    -webkit-transition: height 100s;
+    transition:  height 100s;
+}
+
+#textalign {
+    text-align: start;
+    padding-top: 10px
+}
+
+input {
+    border-radius: 1rem;
+}
+
+button {
+    border-radius: 2rem;
+    align-self: auto;
+}
+div.container {
+    padding: 20px;
+    font-family: 'Nunito';
+    font-size: 20px;
+    width: 500px;
+    -webkit-transition: height 2s;
+    transition: height 2s;
+
+}
+</style>
